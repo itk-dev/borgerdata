4763ba4600e988a12cb529d8df08f9df6aba0d69
Lars Steen <lats@aarhus.dk>
2020-02-13 09:08:10 +0100

    KON-345: Add citizen with unknown address to the seed file

--- seeds/addBorgere.js
+++ seeds/addBorgere.js
@@ -7,7 +7,8 @@ exports.seed = function(knex) {
       return knex(process.env.SQL_SCHEMA + '.' + process.env.SQL_TABLE).insert([
         {CPR: 1234567891, Fornavn: 'Hans', Efternavn: 'Hansen', Vejnavn: 'Testvej', Husnr: '11', Postdistrikt: 'Roskilde', Postnummer: '4000'},
         {CPR: 3213214567, Fornavn: 'Peter', Efternavn: 'Petersen', Vejnavn: 'Kiddevej', Husnr: '56', Postdistrikt: 'Kolding', Postnummer: '6000'},
-        {CPR: 7823492345, Fornavn: 'Merete', Efternavn: 'Finnsen', Vejnavn: 'Skovvej', Husnr: '3', Postdistrikt: 'Odense', Postnummer: '5000'}
+        {CPR: 7823492345, Fornavn: 'Merete', Efternavn: 'Finnsen', Vejnavn: 'Skovvej', Husnr: '3', Postdistrikt: 'Odense', Postnummer: '5000'},
+        {CPR: 1122334455, Fornavn: 'Ulla', Efternavn: 'Kendtsen', Vejnavn: 'Uden Fast Bopæl', Postdistrikt: 'Ukendt postdistrikt', Postnummer: '9999'}
       ]);
     });
 };
